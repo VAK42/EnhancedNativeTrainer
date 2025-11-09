ce8062e87e9cac8d0e0116b97972f2df5104fcd2
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-11-09 12:46:43 +0700

    .

--- EnhancedNativeTrainer/keyboard.cpp
+++ EnhancedNativeTrainer/keyboard.cpp
@@ -12,7 +12,7 @@ void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scanCode, BOOL isExtended, 
 {
 	if (key < KEYS_SIZE)
 	{
-		keyStates[key].time = GetTickCount();
+		keyStates[key].time = GetTickCount64();
 		keyStates[key].isWithAlt = isWithAlt;
 		keyStates[key].wasDownBefore = wasDownBefore;
 		keyStates[key].isUpNow = isUpNow;
@@ -21,11 +21,11 @@ void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scanCode, BOOL isExtended, 
 const int NOW_PERIOD = 100, MAX_DOWN = 5000;
 bool IsKeyDown(DWORD key)
 {
-	return (key < KEYS_SIZE) ? ((GetTickCount() < keyStates[key].time + MAX_DOWN) && !keyStates[key].isUpNow) : false;
+	return (key < KEYS_SIZE) ? ((GetTickCount64() < static_cast<unsigned long long>(keyStates[key].time) + MAX_DOWN) && !keyStates[key].isUpNow) : false;
 }
 bool IsKeyJustUp(DWORD key, bool exclusive)
 {
-	bool b = (key < KEYS_SIZE) ? (GetTickCount() < keyStates[key].time + NOW_PERIOD && keyStates[key].isUpNow) : false;
+	bool b = (key < KEYS_SIZE) ? (GetTickCount64() < static_cast<unsigned long long>(keyStates[key].time) + NOW_PERIOD && keyStates[key].isUpNow) : false;
 	if (b && exclusive)
 		ResetKeyState(key);
 	return b;
