9407731bc558b49aa26d9781ba300d67896657cb
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-11-09 12:34:25 +0700

    .

--- include/nativeCaller.h
+++ include/nativeCaller.h
@@ -9,34 +9,30 @@ static inline void nativePush(T val)
 	{
 		throw "Error: Value Size > 64B";
 	}
-	*reinterpret_cast<T *>(&val64) = val; // &val + sizeof(dw) - sizeof(val)
+	*reinterpret_cast<T*>(&val64) = val;
 	nativePush64(val64);
 }
-
 template <typename R>
 static inline R invoke(UINT64 hash)
 {
 	nativeInit(hash);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1>
 static inline R invoke(UINT64 hash, T1 P1)
 {
 	nativeInit(hash);
 	nativePush(P1);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2)
 {
 	nativeInit(hash);
 	nativePush(P1);
 	nativePush(P2);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3)
 {
@@ -44,9 +40,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3)
 	nativePush(P1);
 	nativePush(P2);
 	nativePush(P3);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4)
 {
@@ -55,9 +50,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4)
 	nativePush(P2);
 	nativePush(P3);
 	nativePush(P4);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5)
 {
@@ -67,9 +61,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5)
 	nativePush(P3);
 	nativePush(P4);
 	nativePush(P5);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6)
 {
@@ -80,9 +73,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6)
 	nativePush(P4);
 	nativePush(P5);
 	nativePush(P6);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7)
 {
@@ -94,9 +86,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P5);
 	nativePush(P6);
 	nativePush(P7);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8)
 {
@@ -109,9 +100,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P6);
 	nativePush(P7);
 	nativePush(P8);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9)
 {
@@ -125,9 +115,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P7);
 	nativePush(P8);
 	nativePush(P9);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10)
 {
@@ -142,9 +131,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P8);
 	nativePush(P9);
 	nativePush(P10);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11)
 {
@@ -160,9 +148,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P9);
 	nativePush(P10);
 	nativePush(P11);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12)
 {
@@ -179,9 +166,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P10);
 	nativePush(P11);
 	nativePush(P12);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13)
 {
@@ -199,9 +185,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P11);
 	nativePush(P12);
 	nativePush(P13);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14)
 {
@@ -220,9 +205,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P12);
 	nativePush(P13);
 	nativePush(P14);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15)
 {
@@ -242,9 +226,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P13);
 	nativePush(P14);
 	nativePush(P15);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16)
 {
@@ -265,9 +248,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P14);
 	nativePush(P15);
 	nativePush(P16);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17)
 {
@@ -289,9 +271,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P15);
 	nativePush(P16);
 	nativePush(P17);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17, T18 P18)
 {
@@ -314,9 +295,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P16);
 	nativePush(P17);
 	nativePush(P18);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17, T18 P18, T19 P19)
 {
@@ -340,9 +320,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P17);
 	nativePush(P18);
 	nativePush(P19);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19, typename T20>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17, T18 P18, T19 P19, T20 P20)
 {
@@ -367,9 +346,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P18);
 	nativePush(P19);
 	nativePush(P20);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19, typename T20, typename T21>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17, T18 P18, T19 P19, T20 P20, T21 P21)
 {
@@ -395,9 +373,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P19);
 	nativePush(P20);
 	nativePush(P21);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19, typename T20, typename T21, typename T22>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17, T18 P18, T19 P19, T20 P20, T21 P21, T22 P22)
 {
@@ -424,9 +401,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P20);
 	nativePush(P21);
 	nativePush(P22);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19, typename T20, typename T21, typename T22, typename T23>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17, T18 P18, T19 P19, T20 P20, T21 P21, T22 P22, T23 P23)
 {
@@ -454,9 +430,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P21);
 	nativePush(P22);
 	nativePush(P23);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19, typename T20, typename T21, typename T22, typename T23, typename T24>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17, T18 P18, T19 P19, T20 P20, T21 P21, T22 P22, T23 P23, T24 P24)
 {
@@ -485,9 +460,8 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P22);
 	nativePush(P23);
 	nativePush(P24);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
-
 template <typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19, typename T20, typename T21, typename T22, typename T23, typename T24, typename T25>
 static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7 P7, T8 P8, T9 P9, T10 P10, T11 P11, T12 P12, T13 P13, T14 P14, T15 P15, T16 P16, T17 P17, T18 P18, T19 P19, T20 P20, T21 P21, T22 P22, T23 P23, T24 P24, T25 P25)
 {
@@ -517,5 +491,5 @@ static inline R invoke(UINT64 hash, T1 P1, T2 P2, T3 P3, T4 P4, T5 P5, T6 P6, T7
 	nativePush(P23);
 	nativePush(P24);
 	nativePush(P25);
-	return *reinterpret_cast<R *>(nativeCall());
+	return *reinterpret_cast<R*>(nativeCall());
 }
