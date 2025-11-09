474ce1b8b0c54bd54cb418d6427a89ffbd407671
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-11-09 12:27:47 +0700

    .

--- include/main.h
+++ include/main.h
@@ -2,9 +2,9 @@
 #include <windows.h>
 #define IMPORT __declspec(dllimport)
 
-IMPORT int createTexture(const char *texFileName);
+IMPORT int createTexture(const char* texFileName);
 IMPORT void drawTexture(int id, int index, int level, int time, float sizeX, float sizeY, float centerX, float centerY, float posX, float posY, float rotation, float screenHeightScaleFactor, float r, float g, float b, float a);
-typedef void (*PresentCallback)(void *);
+typedef void (*PresentCallback)(void*);
 IMPORT void presentCallbackRegister(PresentCallback cb);
 IMPORT void presentCallbackUnregister(PresentCallback cb);
 typedef void (*KeyboardHandler)(DWORD, WORD, BYTE, BOOL, BOOL, BOOL, BOOL);
@@ -20,12 +20,12 @@ IMPORT void nativePush64(UINT64 val);
 IMPORT PUINT64 nativeCall();
 static void WAIT(DWORD time) { scriptWait(time); }
 static void TERMINATE() { WAIT(MAXDWORD); }
-IMPORT UINT64 *getGlobalPtr(int globalId);
-IMPORT int worldGetAllVehicles(int *arr, int arrSize);
-IMPORT int worldGetAllPeds(int *arr, int arrSize);
-IMPORT int worldGetAllObjects(int *arr, int arrSize);
-IMPORT int worldGetAllPickups(int *arr, int arrSize);
-IMPORT BYTE *getScriptHandleBaseAddress(int handle);
+IMPORT UINT64* getGlobalPtr(int globalId);
+IMPORT int worldGetAllVehicles(int* arr, int arrSize);
+IMPORT int worldGetAllPeds(int* arr, int arrSize);
+IMPORT int worldGetAllObjects(int* arr, int arrSize);
+IMPORT int worldGetAllPickups(int* arr, int arrSize);
+IMPORT BYTE* getScriptHandleBaseAddress(int handle);
 enum eGameVersion : int
 {
 	VER_1_0_335_2_STEAM,
