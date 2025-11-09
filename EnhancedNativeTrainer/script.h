a1a63550c83339323b5a09fb2ffa7960f25435c9
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-11-09 12:36:32 +0700

    .

--- EnhancedNativeTrainer/script.h
+++ EnhancedNativeTrainer/script.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "..\..\inc\natives.h"
-#include "..\..\inc\types.h"
-#include "..\..\inc\enums.h"
-#include "..\..\inc\main.h"
+#include "..\include\natives.h"
+#include "..\include\types.h"
+#include "..\include\enums.h"
+#include "..\include\main.h"
 
 void ScriptMain();
